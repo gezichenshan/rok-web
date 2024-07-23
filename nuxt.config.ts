0eb0fb75bbbeb3df784308eeae7be3a3c1f54490
gezichenshan YU <xtyujd@gmail.com>
2024-07-23 13:17:47 +0800

    meta head加不准用户scalable的代码

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,16 @@ export default defineNuxtConfig({
   srcDir: "src",
   modules: ["@ant-design-vue/nuxt", "@unocss/nuxt"],
   css: ["@/assets/reboot.css"],
+  app: {
+    head: {
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1.0,maximum-scale=1.0,user-scalable=0",
+        },
+      ],
+    },
+  },
 });
