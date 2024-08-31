9b9b40d736730455009cce0f2f1f9f9dafb3968c
gezichenshan <276489620@qq.com>
2024-08-31 14:18:05 +0800

    change api base

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const request = axios.create({
-  baseURL: "https://rok.3mir.cc/api/",
+  baseURL: "https://api-rok.3mir.cc/api/",
   // baseURL: "http://localhost:3019/api/",
   timeout: 1000,
   headers: { countryCode: "foobar" },
